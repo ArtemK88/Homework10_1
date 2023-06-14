64d7d3bb93afb4512fd08b022a282279553b0d60
ArtemK88 <kapiton2007@gmail.com>
2023-06-14 21:54:46 +0300

    Homework

--- src/ArrayListCircle.java
+++ src/ArrayListCircle.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ArrayListCircle {
+import java.util.ArrayList;
+
+public class ArrayListCircle {
+    public static void main(String[] args) {
+        int N = 10000;
+
+        ArrayList<Integer> circle = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            circle.add(i);
+        }
+
+        int index = 0;
+        while (circle.size() > 1) {
+            index = (index + 1) % circle.size();
+            circle.remove(index);
+        }
+
+        System.out.println("Залишилась людина з номером: " + circle.get(0));
+    }
 }
+
--- src/LinkedListCircle.java
+++ src/LinkedListCircle.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class LinkedListCircle {
+import java.util.LinkedList;
+
+public class LinkedListCircle {
+    public static void main(String[] args) {
+        int N = 10000;
+
+        LinkedList<Integer> circle = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            circle.add(i);
+        }
+
+        int index = 0;
+        while (circle.size() > 1) {
+            index = (index + 1) % circle.size();
+            circle.remove(index);
+        }
+
+        System.out.println("Залишилась людина з номером: " + circle.get(0));
+    }
 }
+
